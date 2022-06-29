ca30d810f207ba33948abc26f1b77bc7ee6acf14
QurbanovRamiz <qramizq123@gmail.com>
2022-06-29 18:08:54 +0400

    added index.html style.css main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+// for(var i= 0 ;i<5;i++ ){
+//    console.log(i)
+// }
+// var a= 0
+// while(a<5){
+//     a++
+//     console.log(a)
+// }
+//  var num =5;
+//  var name ="ramiz";
+//  var isMarried = false;
+//  console.log(isMarried)
+//  console.log(num)
+//  console.log(name)
+for (var a= 10 ; a>0; a-=2){
+    console.log(a+2)
+}
